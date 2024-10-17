7169d995ed89f08444fd538daf1e2ed0cbca773c
98jungwoo <jungwoo3179@naver.com>
2024-10-18 00:35:02 +0900

    @Service 추가

--- src/main/java/woo/cost/mgmt/services/admin/AdminServiceImp.java
+++ src/main/java/woo/cost/mgmt/services/admin/AdminServiceImp.java
@@ -3,10 +3,11 @@ package woo.cost.mgmt.services.admin;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import woo.cost.mgmt.models.admin.AdminDao;
 import woo.cost.mgmt.models.admin.AdminDto;
-
+@Service
 public class AdminServiceImp implements AdminService{
 
     private final AdminDao adminDao;
