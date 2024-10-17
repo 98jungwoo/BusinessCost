1533304acf2d9266cacefb4c7464c3af6327cf12
98jungwoo <jungwoo3179@naver.com>
2024-10-17 23:59:55 +0900

    create admin 조회

--- src/main/java/woo/cost/mgmt/services/admin/AdminServiceImp.java
+++ src/main/java/woo/cost/mgmt/services/admin/AdminServiceImp.java
@@ -1,5 +1,24 @@
 package woo.cost.mgmt.services.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import woo.cost.mgmt.models.admin.AdminDao;
+import woo.cost.mgmt.models.admin.AdminDto;
+
 public class AdminServiceImp implements AdminService{
+
+    private final AdminDao adminDao;
+
+    @Autowired
+    public AdminServiceImp(AdminDao adminDao){
+        this.adminDao = adminDao;
+    }
+
+    @Override
+    public List<AdminDto> adminSelectAll() {
+        return adminDao.adminSelectAll();
+    }
 
 }
